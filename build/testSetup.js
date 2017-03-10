90f6f9c4a147214b08d77ed584dec481188037e6
Itai Koren <itkoren@gmail.com>
2017-03-10 19:56:09 +0200

    add unit tets

---
+++ build/testSetup.js
@@ -0,0 +1,7 @@
+// this file is not transpiled, so must use commonjs and es5
+
+// register babel to transpile tests before running them
+require('babel-register')();
+
+// disable webpack features that mocha does not understand
+require.extensions['.css'] = () => {};
---
+++ src/index.test.js
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import jsdom from 'jsdom';
+import path from 'path';
+import fs from 'fs';
+
+describe('Our first test', () => {
+  it('Should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
+
+describe('index.html', () => {
+  it('Should say hello', (done) => {
+    const index = fs.readFileSync(path.join('src', 'index.html'), 'utf-8');
+    jsdom.env(index, (err, window) => {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal('Hello World!');
+      window.close();
+      done();
+    });
+  });
+});
